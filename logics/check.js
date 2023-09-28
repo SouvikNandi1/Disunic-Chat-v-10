8da8957a334edc2c54807f931e62d85d36ea275d
Souvik Nandi <83856385+SouvikNandi2004@users.noreply.github.com>
2023-09-28 23:16:02 +0530

    Add files via upload

--- logics/check.js
+++ logics/check.js
@@ -6,3 +6,11 @@ function check1(){
         window.open("/chat/index.html","_self")
     }
 }
+function check2(){
+    if (localStorage.getItem("userId") === null){
+        window.open("/login","_self")
+    }
+    else{
+        console.log("ok")
+    }
+}
